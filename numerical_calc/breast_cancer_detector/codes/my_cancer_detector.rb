e807c3345f46330f7a66a8f87b4d5e610f851ff2
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2017-11-18 17:00:50 +0900

    rev some ipynbs

--- numerical_calc/breast_cancer_detector/codes/my_cancer_detector.rb
+++ numerical_calc/breast_cancer_detector/codes/my_cancer_detector.rb
@@ -1,114 +1,41 @@
-require 'narray'
-def read_n_data_from_lines(matrix_A, vector_b, lines)
-  m,n =  matrix_A.shape
-  lines[0..n-1].each_with_index do |line,i|
-    row = line.chomp.split(',')
-    vector_b[i] = (row[1] == 'B')? 1.0 : -1.0
-    row[2..-1].each_with_index { |ele, j|
-      matrix_A[j,i] = ele.to_f
-    }
-  end
-end
-
-def calc_dLw(matrix_A, vector_b, vector_w, opts)
-  # calc L(w) from A,b and w
-  m, n = matrix_A.shape
-  vector_lw = NArray.sfloat(m)
-  sum, correct = 0, 0
-  n.times do |i|
-    tmp = (matrix_A[0..-1,i] * vector_w)[0]
-    tmp = (tmp<0)? -1.0 : 1.0 if opts[:step]
-    correct += 1 if tmp*vector_b[i]>0
-    tmp = tmp - vector_b[i]
-    sum += tmp*tmp
-  end
-
-  print "L(w) = #{sum}\ncorrect = #{correct}/#{n}\n" if opts[:print]
-
-  # calc dL(w)
-  vector_Lw = NVector.sfloat(m)
-  m.times do |j|
-    n.times do |i|
-      tmp = (matrix_A[0..-1,i] * vector_w)[0]
-      tmp = (tmp<0)? -1.0 : 1.0 if opts[:step]
-      tmp = tmp - vector_b[i]
-      vector_Lw[j] += tmp*matrix_A[j,i]
-    end
-  end
-  return vector_Lw
-end 
-
-def print_w(vector_w)
-  params = ["radius", "texture", "perimeter","area","smoothness","compactness",
-            "concavity","concave points","symmetry","fractal dimension"];
-  print("      params      :      (mean)    (stderr)     (worst)\n")
-  j = 0
-  vector_w.each do |w_j|
-    printf("%17s :",params[j/3]) if j%3==0
-    printf("%12.4f", w_j)
-    print "\n" if j%3==2
-    j += 1
-  end
-end
-
+#require 'narray'
+require 'nmatrix/lapacke'
+#require 'nmatrix'
 # initial set ups
-data_dir = '../Klein_codes'
-lines = File.readlines(File.join(data_dir,'train.data'))
+dir = '.' #'./codes'
+lines_A = File.readlines(File.join(dir, 'train_A.data'))
+lines_b = File.readlines(File.join(dir, 'train_b.data'))
 
-p n = lines.size
-p m = lines[0].split(',').size - 2
-
-matrix_A = NMatrix.sfloat(m,n)
-vector_b = NVector.sfloat(n)
-vector_w = NVector.sfloat(m)
-vector_Lw = NVector.sfloat(m)
-
-read_n_data_from_lines(matrix_A, vector_b, lines)
+p n = lines_A.size
+p m = lines_A[0].split("\t").size
+matrix_A = NMatrix.new([n,m], dtype: :float32,stype: :dense)
+vector_b = NMatrix.new([n], dtype: :float32,stype: :dense)
+vector_w = NMatrix.new([m], dtype: :float32,stype: :dense)
 
 n.times do |i|
-  m.times do |j|
-    vector_Lw[j] += matrix_A[j,i]/n
+  lines_A[i].split("\t").each_with_index do |v,j|
+    matrix_A[i,j] = v.to_f
+  end
+  vector_b[i] = lines_b[i].to_f
+end
+
+def print_w(vector_w)
+  params = ["radius", "texture","perimeter","area",
+            "smoothness","compactness","concavity","concave points",
+            "symmetry","fractal dimension"];
+  print("    (params)      :")
+  print("    (mean)    (stderr)     (worst)")
+  params.each_with_index do |param, i|
+    printf("\n%17s :",param)
+    3.times{|j| printf("%12.8f", vector_w[i*3+j])}
   end
 end
-m.times do |j|
-  vector_w[j] = 1.0
-  #  vector_w[j] = 0.0
-  # vector_w[j] = 1.0/vector_Lw[j]/30
-end
 
-print "# Finding minimum loop\n"
-#loop, sigma = 300, 3.0*10**(-9)
-loop, sigma = 100, 3.0*10**(-9)
-printf("* Loop: %5d\n* sigma: %11.4e\n",loop,sigma)
-step_sel = false
-printf("* Use step function: %s\n\n", step_sel.to_s)
-print "## Initial values of Vector w\n"
-print_w(vector_w)
-vector_Lw = calc_dLw(matrix_A, vector_b, vector_w, opts={:print => true, :step => step_sel})
-print_w(vector_Lw)
-loop.times do |l|
-  print "." if l%10==0
-  #  print "#{i}-loop\n"
-  vector_w = vector_w - vector_Lw*sigma
-  vector_Lw = calc_dLw(matrix_A, vector_b, vector_w, opts={:print => false, :step => step_sel})
-end
-print "\n"
+qq, rr = matrix_A.factorize_qr
+q = qq[0..n-1,0..m-1]
+r = rr[0..m-1,0..m-1]
 
-print "## Adjusted values of Vector w\n"
-print_w(vector_w)
-vector_Lw = calc_dLw(matrix_A, vector_b, vector_w, opts={:print => true, :step => step_sel})
+print(q.transpose.shape)
+print(vector_b.shape)
+q.transpose*vector_b
 
-# validate data judgement
-print "# Validate.data results\n"
-lines = File.readlines(File.join(data_dir,'validate.data'))
-
-n = lines.size
-m = lines[0].split(',').size - 2
-
-matrix_A = NMatrix.sfloat(m,n) #
-vector_b = NVector.sfloat(n) # int
-
-read_n_data_from_lines(matrix_A, vector_b, lines)
-
-#print_w(vector_w)
-vector_Lw = calc_dLw(matrix_A, vector_b, vector_w, opts={:print => true, :step => step_sel})
