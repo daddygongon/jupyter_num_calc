9eb0842b122e100249c32b331ba505cea82f7b80
Shigeto R. Nishiani <daddygongon@users.noreply.github.com>
2022-10-14 13:25:09 +0900

    rev Rakefile

--- numerical_calc/C2Error_c/strange_small_diff.c
+++ numerical_calc/C2Error_c/strange_small_diff.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <math.h>
-#define EPSILON 0.0001
-int main(void){
-  float x=77777,y=7,y1,z,z1;
-  
-  y1=1/y;
-  z=x/y;
-  z1=x*y1;
-  printf("%10.5f %10.5f\n", z,z1);
-  //  if (z!=z1){
-  if (fabs(z-z1)>EPSILON){
+#define EPSILON 0.001
+int main(void)
+{
+  float x = 77777, y = 7, y1, z, z1;
+
+  y1 = 1 / y;
+  z = x / y;
+  z1 = x * y1;
+  printf("%10.2f %10.2f\n", z, z1);
+  // if (z != z1)
+  if (fabs(z - z1) > EPSILON)
+  {
     printf("z is not equal to z1.\n");
-  } else {
+  }
+  else
+  {
     printf("z is equal to z1.\n");
   }
 }
