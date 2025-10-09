f0172466524bd2cb97b3278be3f12ec869c798e8
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2025-10-09 11:24:08 +0900

    del old files

--- deep_learning_from_scratch/ch03/load_mnist.py
+++
@@ -1,8 +0,0 @@
-import sys, os
-sys.path.append(os.pardir)
-from dataset.mnist import load_mnist
-
-(x_train, t_train), (x_test, t_test) = \
-    load_mnist(flatten=True, normalize=False)
-
-print(x_train.shape)
